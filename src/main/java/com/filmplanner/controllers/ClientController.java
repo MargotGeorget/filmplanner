0a5b23b3a1ac0169159c885bba44e1b22fedde99
Margot <margot-georget@hotmail.fr>
2021-12-06 12:38:32 +0100

    Added mouse event in client's list

--- src/main/java/com/filmplanner/controllers/ClientController.java
+++ src/main/java/com/filmplanner/controllers/ClientController.java
@@ -2,10 +2,12 @@ package com.filmplanner.controllers;
 
 import javafx.collections.FXCollections;
 import javafx.collections.transformation.FilteredList;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
 import javafx.util.Pair;
 
 import java.net.URL;
@@ -32,6 +34,18 @@ public class ClientController implements Initializable {
             pairs.add(new Pair<>("Client "+ i, "Adress client"));
         }
         this.clientsList.setItems(new FilteredList<>(FXCollections.observableList(pairs)));
+        this.clientsList.setOnMouseClicked(new EventHandler<MouseEvent>(){
+
+            @Override
+            public void handle (MouseEvent event){
+                //TODO: ouvrir une nouvelle fenêtre
+                //TODO: appeler facade avec findClintById() et afficher les informations du client dans la nouvelle fenêtre
+                System.out.println("clicked on " + clientsList.getSelectionModel().getSelectedItem());
+            }
+
+        });
     }
+
+
 
 }
