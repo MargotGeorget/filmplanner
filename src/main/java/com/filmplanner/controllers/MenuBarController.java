4675c6746a44283b66a466277262397948e3f490
Margot <margot-georget@hotmail.fr>
2021-12-13 08:34:09 +0100

    Refactoring menu bar

---
+++ src/main/java/com/filmplanner/controllers/MenuBarController.java
@@ -0,0 +1,33 @@
+package com.filmplanner.controllers;
+
+import com.filmplanner.App;
+import javafx.fxml.FXML;
+import javafx.scene.control.MenuBar;
+
+import java.io.IOException;
+
+public class MenuBarController {
+
+    @FXML
+    private MenuBar addClient;
+
+    @FXML
+    private MenuBar viewClients;
+
+    @FXML
+    private MenuBar addProject;
+
+    @FXML
+    private MenuBar viewProjects;
+
+    public MenuBarController() {
+    }
+
+    public void addClientAction() throws IOException {
+        App.setRoot("views/client/clientForm");
+    }
+
+    public void viewClientsAction() throws IOException {
+        App.setRoot("views/client/clientView");
+    }
+}
