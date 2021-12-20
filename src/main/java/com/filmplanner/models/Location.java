a9473f98c26b1f56d8f619ef8242fc8270131734
Margot <margot-georget@hotmail.fr>
2021-12-20 15:17:45 +0100

    Added shoot model and location

---
+++ src/main/java/com/filmplanner/models/Location.java
@@ -0,0 +1,50 @@
+package com.filmplanner.models;
+
+public class Location {
+    //Attributes
+    private int streetNumber;
+    private String street;
+    private String city;
+    private String zipCode;
+
+    //Constructor:
+    public Location(int streetNumber, String street, String city, String zipCode) {
+        this.setStreetNumber(streetNumber);
+        this.setStreet(street);
+        this.setCity(city);
+        this.setZipCode(zipCode);
+    }
+
+    //Getters and setters:
+    public int getStreetNumber() {
+        return streetNumber;
+    }
+
+    public void setStreetNumber(int streetNumber) {
+        this.streetNumber = streetNumber;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getZipCode() {
+        return zipCode;
+    }
+
+    public void setZipCode(String zipCode) {
+        this.zipCode = zipCode;
+    }
+}
--- src/main/java/com/filmplanner/models/Shoot.java
+++ src/main/java/com/filmplanner/models/Shoot.java
@@ -1,4 +1,102 @@
 package com.filmplanner.models;
 
+import java.util.*;
+
 public class Shoot {
+    //Attributes:
+    private String name;
+    private String description;
+    private Date startDate;
+    private Date endDate;
+    private Location location;
+    private Map<User, String> members; //TODO: Changer string par role
+    private List<Gear> gears;
+    private Project project;
+
+    //Constructors:
+    public Shoot(String name, String description, Date startDate, Date endDate, Location location, Map<User, String> members, ArrayList<Gear> gears, Project project) {
+        this.name = name;
+        this.description = description;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.location = location;
+        this.members = members;
+        this.gears = gears;
+        this.project = project;
+    }
+
+    public Shoot(String name, String description, Date startDate, Date endDate, Location location, Project project) {
+        this(name,description,startDate, endDate, location, new HashMap<>(), new ArrayList<>(), project);
+        this.name = name;
+        this.description = description;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.location = location;
+        this.project = project;
+    }
+
+    //Getters and setters:
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+
+    public Date getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(Date endDate) {
+        this.endDate = endDate;
+    }
+
+    public Location getLocation() {
+        return location;
+    }
+
+    public void setLocation(Location location) {
+        this.location = location;
+    }
+
+    public Map<User, String> getMembers() {
+        return members;
+    }
+
+    public void setMembers(Map<User, String> members) {
+        this.members = members;
+    }
+
+    public List<Gear> getGears() {
+        return gears;
+    }
+
+    public void setGears(List<Gear> gears) {
+        this.gears = gears;
+    }
+
+    public Project getProject() {
+        return project;
+    }
+
+    public void setProject(Project project) {
+        this.project = project;
+    }
 }
